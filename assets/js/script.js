cbb68c443f08dea0b5eb1fc06d670447602a1e52
Nick Melanson <nickm890@gmail.com>
2022-02-16 18:49:05 -0500

    feature done

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,52 @@ var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (e) {
+    console.log("activate", this);
+  },
+  deactivate: function (e) {
+    console.log("deactivate", this);
+  },
+  over: function (e) {
+    console.log("over", e.target);
+  },
+  out: function (e) {
+    console.log("out", e.target);
+  },
+  update: function (e) {
+    // array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this)
+      .children()
+      .each(function () {
+        var text = $(this).find("p").text().trim();
+        var date = $(this).find("span").text().trim();
+
+        // add task data to the temp array as an object
+        tempArr.push({
+          text: text,
+          date: date,
+        });
+      });
+
+    // trim down list's ID to match object property
+    var arrName = $(this).attr("id").replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+
+    console.log(tempArr);
+  },
+});
+
 $(".list-group").on("click", "p", function () {
   var text = $(this).text().trim();
   var textInput = $("<textarea>").addClass("form-control").val(text);
@@ -151,6 +197,21 @@ $("#remove-tasks").on("click", function () {
     $("#list-" + key).empty();
   }
   saveTasks();
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (e, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function (e, ui) {
+    console.log("over");
+  },
+  out: function (e, ui) {
+    console.log("out");
+  },
 });
 
 // load tasks for the first time
